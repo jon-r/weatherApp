a052fdccfac580c62bf9682e27e411c124c9cb0b
Jon <jonr.web@gmail.com>
2015-11-24 00:37:25 +0000

    work on strings

--- app/components/weather/weatherService.js
+++ app/components/weather/weatherService.js
@@ -23,14 +23,62 @@ app.filter('secToMs', function() {
 
 //day string
 app.factory('dayStr', ['$filter', function($filter) {
+
+  Number.prototype.between = function(a,b) {
+    return this > a && this < b;
+  }
+
+  var setStrings = function(dayArr) {
+    //var dayX = dayArr.length;
+    return {
+      windVal: function() {
+        var beaufort = [
+           0,    //none
+           0.3, //calm
+           1.5, //air
+           3.3, //light
+           5.5, //gentle
+           8.0, //moderate
+          10.8, //fresh
+          13.9, //strong
+          17.2, //high
+          20.7, //gale
+          24.5, //severe
+          28.4, //storm
+          32.6  //violent
+        ];
+
+        var winds = beaufort.length;
+        var wind = dayArr.speed;
+
+        for (i=0;i<winds;i++) {
+          j = i - 1;
+          if (wind.between(beaufort[j], beaufort[i])) {
+            return i;
+            break;
+          }
+        }
+
+      },
+      overview: function() {
+        return dayArr.weather[0].description;
+      },
+    }
+  }
+
+
+
+
+
   return function(days) {
     count = days.length;
     out = new Array(count);
 
-    for (i=0;i<count;i++) {
-      dt = (days[i].dt * 1000);
+    for (x=0;x<count;x++) {
+      day = setStrings(days[x]);
+      dt = (days[x].dt * 1000);
       var today = $filter('date')(dt,'EEEE');
-      out[i] = today + '\'s weather will be ' + days[i].weather[0].description;
+      out[x] = today + '\'s weather will be ' + day.overview() + '. The wind level is ' + day.windVal();
     }
     console.log(out);
   }
